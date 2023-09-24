45ad6ac2348ce3ac6d850e69e2048135bcb37ce0
Giorgos <giorgos.k.kalligiannakis@gmail.com>
2023-09-24 20:49:34 +0300

    fix for logger

--- index.js
+++ index.js
@@ -2,20 +2,26 @@ import express from 'express';
 import { logger } from './src/services/logger/index.js';
 
 const app = express();
+const CONFIG = {
+	port: 5001,
+};
 
 const state = {
 	count: 0,
 };
-app.listen(5001, () => console.log('api running on 5001'));
+
+app.listen(CONFIG.port, () => {
+	logger.info(`server started on port ${CONFIG.port}`);
+});
 
 app.get('/', (req, res) => {
-	logger.log('endpoint / called');
+	logger.info(`handler for /`);
 	state.count += 1;
 	res.json({ state });
 });
 
 app.get('/p2', (req, res) => {
-	logger.log('endpoint /p2 called');
+	logger.info(`handler for /p2`);
 	state.count += 2;
 	res.json({ state });
 });
--- src/services/logger/index.js
+++ src/services/logger/index.js
@@ -1,12 +1,8 @@
 import winston from 'winston';
 
 export const logger = winston.createLogger({
-  level: 'info',
-  format: winston.format.json(),
-  defaultMeta: { service: 'user-service' },
-  transports: [
-    new winston.transports.File({ filename: 'error.log', level: 'error' }),
-    new winston.transports.File({ filename: 'combined.log' }),
-  ],
+	level: 'silly',
+	format: winston.format.json(),
+	defaultMeta: { service: 'user-service' },
+	transports: [new winston.transports.File({ filename: 'combined.log' })],
 });
-
