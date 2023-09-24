f3d519b8f0d60ec4970f61b9e793ee48e0afa949
Giorgos <giorgos.k.kalligiannakis@gmail.com>
2023-09-24 13:24:19 +0300

    stpd state

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ import express from 'express';
 
 const app = express();
 
+const state = {
+	count: 0,
+};
 app.listen(5001, () => console.log('api running on 5001'));
 
-app.get('/', (req, res) => res.json('my api is running!!!'));
+app.get('/', (req, res) => {
+	state.count += 1;
+	res.json(state);
+});
